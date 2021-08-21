26bf8d72859dc693d231b805d7f170418a865e63
Guillermo_O_C <josuegorellanac@gmail.com>
2021-08-21 13:29:36 -0600

    Inicio Backend

---
+++ creds_template.js
@@ -0,0 +1,9 @@
+let aws_keys = {
+    s3: {
+        region: 'us-east-2',
+        accessKeyId: "AKIA55OLYKFC43WK322N",
+        secretAccessKey: "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",
+        //apiVersion: '2006-03-01',
+    }
+}
+module.exports = aws_keys
